974e04d7a0e870162bafdf32c611fad4366ac9a3
contra.chen <1095695904@qq.com>
2021-12-08 18:38:31 +0800

    可以实现imagebutton的各项功能了！！！

--- app/src/main/java/com/example/mealsmanager/MainActivity.java
+++ app/src/main/java/com/example/mealsmanager/MainActivity.java
@@ -6,21 +6,43 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView pic;
-    ImageButton ib,ib_add,ib_del;
+    ImageButton ib,ib_add,ib_del,ib_modify,ib_clc;
+
+    private MyDAO myDAO;  //数据库访问对象
+    private ListView listView;
+    private List<Map<String,Object>> listData;
+    private Map<String,Object> listItem;
+    private SimpleAdapter listAdapter;
+    private EditText et_ms;  //数据表包含3个字段，第1字段为自增长类型
+    private EditText et_xf;
+    private EditText et_rl;
+    private EditText et_date;
+    private  String selId=null;  //选择项id
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,24 +50,128 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ib=(ImageButton) findViewById(R.id.ib);ib.setOnClickListener(this);
         ib_add=(ImageButton)findViewById(R.id.ib_add);ib_add.setOnClickListener(this);
         ib_del=(ImageButton)findViewById(R.id.ib_del);ib_del.setOnClickListener(this);
+        ib_modify=(ImageButton)findViewById(R.id.ib_modify);ib_modify.setOnClickListener(this);
+        ib_clc=(ImageButton)findViewById(R.id.ib_clc);ib_clc.setOnClickListener(this);
+
+        et_ms=(EditText)findViewById(R.id.et_ms);
+        et_xf=(EditText)findViewById(R.id.et_xf);
+        et_rl=(EditText)findViewById(R.id.et_rl);
+        et_date=(EditText)findViewById(R.id.et_date);
+
+        myDAO = new MyDAO(this);  //创建数据库访问对象
+        if(myDAO.getRecordsNumber()==0) {  //防止重复运行时重复插入记录
+            myDAO.insertInfo("rice",2,300,"1208");   //插入记录
+            myDAO.insertInfo("noodle",4,200,"1209"); //插入记录
+        }
+        displayRecords();   //显示记录
+    }
+
+    public void displayRecords(){  //显示记录方法定义
+        listView = (ListView)findViewById(R.id.lv_food);
+        listData = new ArrayList<Map<String,Object>>();
+        Cursor cursor = myDAO.allQuery();
+        while (cursor.moveToNext()){
+            int id=cursor.getInt(0);  //获取字段值
+            String meal=cursor.getString(1);
+            int cost=cursor.getInt(2);
+            int heat=cursor.getInt(3);
+            String date=cursor.getString(4);
+            //int age=cursor.getInt(2);
+//            int age=cursor.getInt(cursor.getColumnIndex("age"));//推荐此种方式
+            listItem=new HashMap<String,Object>(); //必须在循环体里新建
+            listItem.put("_id", id);  //第1参数为键名，第2参数为键值
+            listItem.put("meal", meal);
+            listItem.put("cost", cost);
+            listItem.put("heat", heat);
+            listItem.put("date", date);
+            listData.add(listItem);   //添加一条记录
+        }
+        listAdapter = new SimpleAdapter(this,
+                listData,
+                R.layout.listview, //自行创建的列表项布局
+                new String[]{"_id","meal","cost","heat","date"},
+                new int[]{R.id.l_id,R.id.l_meal,R.id.l_cost,R.id.l_heat,R.id.l_date});
+        listView.setAdapter(listAdapter);  //应用适配器
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {  //列表项监听
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String,Object> rec= (Map<String, Object>) listAdapter.getItem(position);  //从适配器取记录
+                et_ms.setText(rec.get("meal").toString());  //刷新文本框
+                et_xf.setText(rec.get("cost").toString());
+                et_rl.setText(rec.get("heat").toString());
+                et_date.setText(rec.get("date").toString());
+                Log.i("ly",rec.get("_id").toString());
+                selId=rec.get("_id").toString();  //供修改和删除时使用
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        int id = v.getId();
-        Intent intent=new Intent();
-        switch (id){
-            case R.id.ib:
+        if(selId!=null) {  //选择了列表项后，可以增加/删除/修改
+            String p1 = et_ms.getText().toString().trim();
+            int p2 = Integer.parseInt(et_xf.getText().toString());
+            int p3 = Integer.parseInt(et_rl.getText().toString());
+            String p4 = et_date.getText().toString().trim();
+            switch (v.getId()){
+                case  R.id.ib_add:
+                    myDAO.insertInfo(p1,p2,p3,p4);
+                    break;
+                case  R.id.ib_modify:
+                    myDAO.updateInfo(p1,p2,p3,p4,selId);
+                    Toast.makeText(getApplicationContext(),"更新成功！",Toast.LENGTH_SHORT).show();
+                    break;
+                case  R.id.ib_del:
+                    myDAO.deleteInfo(selId);
+                    Toast.makeText(getApplicationContext(),"删除成功！",Toast.LENGTH_SHORT).show();
+                    et_ms.setText(null);
+                    et_xf.setText(null);
+                    et_rl.setText(null);
+                    et_date.setText(null);
+                    selId=null; //提示
+                    break;
+                case R.id.ib_clc:
+                    Toast.makeText(getApplicationContext(),"输入已清空！",Toast.LENGTH_SHORT).show();
+                    et_ms.setText(null);
+                    et_xf.setText(null);
+                    et_rl.setText(null);
+                    et_date.setText(null);
+                    selId=null;
+                    break;
+            }
+        }else{  //未选择列表项
+            if(v.getId()==R.id.ib_add) {  //单击添加按钮
+                String p1 = et_ms.getText().toString();
+                String p2 = et_xf.getText().toString();
+                String p3 = et_rl.getText().toString();
+                String p4 = et_date.getText().toString();
+                if(p1.equals("")||p4.equals("")){  //要求输入了信息
+                    Toast.makeText(getApplicationContext(),"美食和日期都不能为空！",Toast.LENGTH_SHORT).show();
+                }else{
+                    myDAO.insertInfo(p1, Integer.parseInt(p2),Integer.parseInt(p3),p4);  //第2参数转型
+                }
+            }
+            else if (v.getId()==R.id.ib){
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     //没有权限时请求该权限：出现是否型对话框由用户选择是否授权
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 } else {
                     fun();
                 }
-                break;
-            default:
-                break;
+            }
+            else if (v.getId()==R.id.ib_clc){
+                Toast.makeText(getApplicationContext(),"输入已清空！",Toast.LENGTH_SHORT).show();
+                et_ms.setText(null);
+                et_xf.setText(null);
+                et_rl.setText(null);
+                et_date.setText(null);
+                selId=null;
+            }
+            else{   //单击了修改或删除按钮
+                Toast.makeText(getApplicationContext(),"请先选择记录！",Toast.LENGTH_SHORT).show();
+            }
         }
+        displayRecords();//刷新ListView对象
     }
 
     void get_image(Intent data){
